c79323bef93586c9e462bd6132682c6f6d6119ab
Ido <tihonist@DESKTOP-QAUJMNG>
2026-02-12 15:44:42 +0200

    commit-this is the base version of the code

---
+++ Quests/Group_03/Quest_03.xaml.cs
@@ -0,0 +1,9 @@
+namespace FindShaulsTreasure.Quests.Group_03;
+
+public partial class Quest_03 : BaseQuestView
+{
+    public Quest_03(int teamId) : base(teamId)
+    {
+        InitializeComponent();
+    }
+}
